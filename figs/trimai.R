4ec2f85d0bd14d8932f36dab1db7d1d24440bf91
Brendan Cooley <brendan.cooley@gmail.com>
2019-10-08 15:44:02 -0400

    begin github migration

--- figs/trimai.R
+++
@@ -1,8 +0,0 @@
-trimaiPlot <- ggplot(data=trimaiY, aes(x=TRI, y=MAI, label=iso3)) +
-  geom_point() +
-  geom_text_repel() +
-  xlab('Trade Restrictiveness Index') +
-  ylab('Market Access Index') +
-  ggtitle(paste0('Trade Restrictiveness and Market Access, ', Y)) +
-  coord_fixed() +
-  theme_classic()
