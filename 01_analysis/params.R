94e8a26f326cc00cec1ff28876e84ea9c7bffafb
bcooley <brendan.cooley@gmail.com>
2019-10-16 19:44:52 -0400

    vertical slides progress

--- 01_analysis/params.R
+++ 01_analysis/params.R
@@ -103,7 +103,7 @@ ELLccodes <- c("EST", "LVA", "LTU")
 TRIMAI <- F  # include TRI and MAI?
 cluster <- T  # cluster countries?
 Kmeans <- 3  # number of clusters
-KmeansEUD <- 4  # number of clusters with EU disaggregated
+KmeansEUD <- 3  # number of clusters with EU disaggregated
 
 # revenues
 tauRev <- FALSE
