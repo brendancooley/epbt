50061b88261e0cc20cd4a5e7fcf87d62fde2f4e2
Brendan Cooley <brendan.cooley@gmail.com>
2020-04-21 12:10:21 -0400

    Update params.R

--- 01_code/params.R
+++ 01_code/params.R
@@ -71,10 +71,12 @@ if (EUD == TRUE) {
 }
 if (TPSP == TRUE) {
   bootstrap_tpsp_dir <- paste0(basedir, "05_bootstrap/tpsp/")
-  mkdir(bootstrap_tpsp_dir)
   bootstrap_dir <- paste0(bootstrap_tpsp_dir, size)
   bootstrap_sharesTR_dir <- paste0(bootstrap_dir, "sharesTR/")
   bootstrap_dTR_dir <- paste0(bootstrap_dir, "dTR/")
+  mkdir(bootstrap_tpsp_dir)
+  mkdir(bootstrap_sharesTR_dir)
+  mkdir(bootstrap_dTR_dir)
 }
 
 bootstrap_P_dir <- paste0(bootstrap_dir, "P/")
