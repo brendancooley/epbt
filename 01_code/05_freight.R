dac974d8537690f577f528a46cdf275013dff3a4
Brendan Cooley <brendan.cooley@gmail.com>
2020-05-08 13:59:30 -0400

    logit modes model init

--- 01_code/05_freight.R
+++ 01_code/05_freight.R
@@ -80,7 +80,7 @@ if (bootstrap==TRUE) {
 
 # standardized model for paper, unstandardized for predictions
 seaModelOutput <- lm(avcsea ~ hs2 + bs(year, degree = 3) + seadist_log_scaled + contig, data=flowsSea, weights=flowsSea$val)
-# flowsSea %>% select(avcsea, hs2, year, seadist_log_scaled, contig, everything())
+seaModel <- lm(avcsea ~ hs2 + bs(year, degree = 3) + seadist_log + contig, data=flowsSea, weights=flowsSea$val)
 
 ### Land costs ### 
 flowsLand <- flows %>% filter(!is.na(avcland))
@@ -90,6 +90,7 @@ if (bootstrap==TRUE) {
 }
 
 landModelOutput <- lm(avcland ~ hs2 + bs(year, degree=3) + adist_log_scaled + contig, data=flowsLand, weights=flowsLand$val)
+landModel <- lm(avcland ~ hs2 + bs(year, degree=3) + adist_log + contig, data=flowsLand, weights=flowsLand$val)
 
 ### Air Costs ###
 flowsAir <- flows %>% filter(!is.na(avcair))
@@ -99,105 +100,109 @@ if (bootstrap==TRUE) {
 }
 
 airModelOutput <- lm(avcair ~ hs2 + bs(year, degree=3) + adist_log_scaled + contig, data=flowsAir, weights=flowsAir$val)
+airModel <- lm(avcair ~ hs2 + bs(year, degree=3) + adist_log + contig, data=flowsAir, weights=flowsAir$val)
 
-### MODEL MODE SHARES ###
+### MAP AD VALOREM COST PREDICTIONS
 
-flowsModes <- flows %>% filter(!is.na(airshare)) # %>% filter(airshare + seashare + landshare + othershare >= .01)
-flowsModes$landshare <- ifelse(is.na(flowsModes$landshare), 0, flowsModes$landshare)
-if (bootstrap==TRUE) {
-  flowsModes <- flowsModes %>% group_by(j_iso3) %>% sample_frac(size=1, replace=T) %>% ungroup()
-}
+flowsSeaX <- flows %>% select(hs2, year, seadist_log, contig, i_island, j_island)
+flows$avcseaP <- predict(seaModel, flowsSeaX)
 
-flowsModes$hs2 <- as.factor(flowsModes$hs2)
+flowsAirX <- flows %>% select(hs2, year, adist_log, contig)
+flows$avcairP <- predict(airModel, flowsAirX)
 
-flowsModesX <- flowsModes %>% select(adist_log, seadist_log, contig, year, i_island, j_island, hs2)
-flowsModesXOutput <- flowsModes %>% select(adist_log_scaled, seadist_log_scaled, contig, year, i_island, j_island, hs2)
-flowsModesY <- flowsModes %>% select(airshare, seashare, landshare, othershare) %>% as.matrix()
+flowsLandX <- flows %>% select(hs2, year, adist_log, contig)
+flows$avclandP <- predict(landModel, flowsLandX)
 
-d <- 0
-modesModelOutput <- multinom(flowsModesY ~ hs2 + bs(year, degree=3) + adist_log_scaled + seadist_log_scaled + contig + i_island + j_island, data=flowsModesXOutput, decay=d, trace=FALSE)
+# enforce nonnegativity constraint
+flows$avcairP <- ifelse(flows$avcairP < 0, 0, flows$avcairP)
+flows$avcseaP <- ifelse(flows$avcseaP < 0, 0, flows$avcseaP)
+flows$avclandP <- ifelse(flows$avclandP < 0, 0, flows$avclandP)
 
-if (runPreds == TRUE) {
-  
-  seaModel <- lm(avcsea ~ hs2 + bs(year, degree = 3) + seadist_log + contig, data=flowsSea, weights=flowsSea$val)
-  landModel <- lm(avcland ~ hs2 + bs(year, degree=3) + adist_log + contig, data=flowsLand, weights=flowsLand$val)
-  airModel <- lm(avcair ~ hs2 + bs(year, degree=3) + adist_log + contig, data=flowsAir, weights=flowsAir$val)
-  # summary(airModel)
-  modesModel <- multinom(flowsModesY ~ hs2 + bs(year, degree=3) + adist_log + seadist_log + contig + i_island + j_island, data=flowsModesX, decay=d, trace=FALSE)
-  
-  ### MAP AD VALOREM COST PREDICTIONS
-  
-  flowsSeaX <- flows %>% select(hs2, year, seadist_log, contig, i_island, j_island)
-  flows$avcseaP <- predict(seaModel, flowsSeaX)
-  
-  flowsAirX <- flows %>% select(hs2, year, adist_log, contig)
-  flows$avcairP <- predict(airModel, flowsAirX)
-  
-  flowsLandX <- flows %>% select(hs2, year, adist_log, contig)
-  flows$avclandP <- predict(landModel, flowsLandX)
-  
-  # enforce nonnegativity constraint
-  flows$avcairP <- ifelse(flows$avcairP < 0, 0, flows$avcairP)
-  flows$avcseaP <- ifelse(flows$avcseaP < 0, 0, flows$avcseaP)
-  flows$avclandP <- ifelse(flows$avclandP < 0, 0, flows$avclandP)
-  
-  ### MAP MODE SHARES ###
-  
-  flowsModesP <- predict(modesModel, newdata = flows, type="probs") %>% as_tibble()
-  colnames(flowsModesP) <- paste0(colnames(flowsModesP), "P")
-  
-  # project air, land, sea onto 2d simplex because othershare constitutes small proportion in data
-  flowsModesPals <- flowsModesP %>% select(-one_of("othershareP")) %>% as.matrix()
-  # projsplx_2(flowsModesPals[1, ]) %>% sum()
-  flowsModesPals <- apply(flowsModesPals, 1, projsplx_2) %>% t() %>% as_tibble()
-  # rowSums(test)
-  
-  flows <- bind_cols(flows, flowsModesPals)
-  
-  
-  ### CALCULATE PREDICTED DELTAS ###
-  
-  aggShare <- flows %>% group_by(i_iso3, j_iso3) %>%
-    summarise(
-      air=sum(val*airshareP),
-      sea=sum(val*seashareP),
-      land=sum(val*landshareP),
-      val=sum(val)
-    )
-  aggShare$airshare <- aggShare$air / aggShare$val
-  aggShare$seashare <- aggShare$sea / aggShare$val
-  aggShare$landshare <- aggShare$land / aggShare$val
-  
-  aggCost <- flows %>% group_by(i_iso3, j_iso3) %>%
-    summarise(
-      air=weighted.mean(avcairP, val),
-      sea=weighted.mean(avcseaP, val),
-      land=weighted.mean(avclandP, val)
-    )
-  
-  delta <- flows %>% group_by(i_iso3, j_iso3, year) %>%
-    summarise(
-      fob=sum(val),
-      freight=sum(val * (airshareP * avcairP + seashareP * avcseaP + landshareP * avclandP))
-    )
-  delta$cif <- delta$fob + delta$freight
-  delta$avc <- delta$cif / delta$fob
-  
-  if (bootstrap==FALSE) {
-    if (EUD==FALSE) {
-      if (TPSP==FALSE) {
-        write_csv(delta, paste0(cleandir, "delta.csv"))
-      } else {
-        write_csv(delta, paste0(cleandirTPSP, "delta.csv"))
-      }
+### LOGIT DEMAND MODEL ###
+
+
+# project out othershare
+flows_proj <- flows %>% select(seashare, airshare, landshare) %>% as.matrix() %>% apply(1, projsplx) %>% t() %>% as_tibble()
+flows <- flows %>% select(-seashare, -airshare, -landshare, -othershare) %>% bind_cols(flows_proj) 
+
+flows$seashare_diff <- log(flows$seashare) - log(flows$airshare)
+flows$landshare_diff <- log(flows$landshare) - log(flows$airshare)
+
+flows$avcsea_diff <- flows$avcseaP - flows$avcairP
+flows$avcland_diff <- flows$avclandP - flows$avcairP
+
+flows$island_max <- apply(flows %>% select(i_island, j_island), 1, max)
+flows$landlocked_max <- apply(flows %>% select(i_landlocked, j_landlocked), 1, max)
+
+flowsLogitSea <- flows %>% select(seashare_diff, avcsea_diff) %>% filter(!is.na(seashare_diff), !is.infinite(seashare_diff))
+colnames(flowsLogitSea)[1:2] <- c("share_diff", "price_diff")
+flowsLogitSea$sea_indicator <- 1
+flowsLogitSea$land_indicator <- 0
+
+flowsLogitLand <- flows %>% select(landshare_diff, avcland_diff) %>% filter(!is.na(landshare_diff), !is.infinite(landshare_diff))
+colnames(flowsLogitLand)[1:2] <- c("share_diff", "price_diff")
+flowsLogitLand$sea_indicator <- 0
+flowsLogitLand$land_indicator <- 1
+
+flowsLogit <- bind_rows(flowsLogitSea, flowsLogitLand)
+
+flowsLogitModel <- lm(data=flowsLogit, share_diff~price_diff+sea_indicator+land_indicator-1)
+summary(flowsLogitModel)
+beta_price <- coef(flowsLogitModel)[1]
+u_sea <- coef(flowsLogitModel)[2]
+u_land <- coef(flowsLogitModel)[3]
+
+flows$airshare_exp <- 1
+flows$seashare_exp <- exp(flows$avcsea_diff * beta_price + u_sea) * (1 - flows$landlocked_max)
+flows$landshare_exp <- exp(flows$avcland_diff * beta_price + u_land) * (1 - flows$island_max)
+flows$logit_denom <- flows$airshare_exp + flows$seashare_exp + flows$landshare_exp
+
+flows$airshareP <- flows$airshare_exp / flows$logit_denom
+flows$seashareP <- flows$seashare_exp / flows$logit_denom
+flows$landshareP <- flows$landshare_exp / flows$logit_denom
+
+### CALCULATE PREDICTED DELTAS ###
+
+aggShare <- flows %>% group_by(i_iso3, j_iso3) %>%
+  summarise(
+    air=sum(val*airshareP),
+    sea=sum(val*seashareP),
+    land=sum(val*landshareP),
+    val=sum(val)
+  )
+aggShare$airshare <- aggShare$air / aggShare$val
+aggShare$seashare <- aggShare$sea / aggShare$val
+aggShare$landshare <- aggShare$land / aggShare$val
+
+aggCost <- flows %>% group_by(i_iso3, j_iso3) %>%
+  summarise(
+    air=weighted.mean(avcairP, val),
+    sea=weighted.mean(avcseaP, val),
+    land=weighted.mean(avclandP, val)
+  )
+
+delta <- flows %>% group_by(i_iso3, j_iso3, year) %>%
+  summarise(
+    fob=sum(val),
+    freight=sum(val * (airshareP * avcairP + seashareP * avcseaP + landshareP * avclandP))
+  )
+delta$cif <- delta$fob + delta$freight
+delta$avc <- delta$cif / delta$fob
+
+if (bootstrap==FALSE) {
+  if (EUD==FALSE) {
+    if (TPSP==FALSE) {
+      write_csv(delta, paste0(cleandir, "delta.csv"))
     } else {
-      write_csv(delta, paste0(cleandirEU, "delta.csv"))
+      write_csv(delta, paste0(cleandirTPSP, "delta.csv"))
     }
   } else {
-    write_csv(delta, paste0(bootstrap_freight_dir, bootstrap_id, ".csv"))
+    write_csv(delta, paste0(cleandirEU, "delta.csv"))
   }
-  print("done")
+} else {
+  write_csv(delta, paste0(bootstrap_freight_dir, bootstrap_id, ".csv"))
 }
+
 
 print("-----")
 print("Concluding 05_freight.R")
